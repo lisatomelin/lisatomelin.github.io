acf99c957d8ce4980f96932e4126119ecfe6c12a
Lisi Tomelin <lisa_tomelin@hotmail.com>
2025-09-24 19:24:28 -0300

    fix: config home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeModule } from './components/home/home.module';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HomeModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
